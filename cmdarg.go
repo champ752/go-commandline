d34e3b6f04c735ee9573d5ea7473d2f68e7ff22d
champ753 <chaiwat.c@cdg.co.th>
2020-10-26 16:53:27 +0700

    add parse argument

--- cmdarg.go
+++ cmdarg.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"goCmd/actions"
 	"goCmd/constant"
@@ -8,59 +9,74 @@ import (
 	"strconv"
 )
 
+
 func InputHandler(firstNumber *int,secondNumber *int) error {
-	fmt.Print("Please enter 1st number: ");
+	fmt.Print("Please enter 1st number: ")
 	_, err := fmt.Scanf("%d\n",firstNumber)
 	if err != nil {
-		fmt.Println("Invalid 1st number")
-		return err
+		return errors.New("invalid 1st number")
 	}
-
-	fmt.Print("Please enter 2nd number: ");
+	fmt.Print("Please enter 2nd number: ")
 	_, err = fmt.Scanf("%d\n",secondNumber)
 	if err != nil {
-		fmt.Println("Invalid 2st number")
-		return err
+		return errors.New("invalid 2nd number")
 	}
 	return nil
 }
-func DisplayMenu()  {
-	fmt.Println("Please select selectedMenu")
-	fmt.Println("1 plus")
-	fmt.Println("2 minus")
+func DisplayMenu() string {
+	outputString := fmt.Sprintf("%s\n%s\n%s","Please select menu" ,"1 plus" ,"2 minus" )
+	return outputString
 }
-func CheckArg(arg *string, selectedMenu *int) {
-	if len(os.Args) > 1 {
-		*arg = os.Args[1]
+func CheckArgument(argumentPlace int) bool {
+	if len(os.Args) > argumentPlace {
+		return true
 	}
-	if *arg != "" {
-		var err error
-		*selectedMenu, err =  strconv.Atoi(*arg)
-		if err != nil {
-			fmt.Println("Invalid input arg")
-			*selectedMenu = 0
+	return false
+}
+func ParseArgumentToMenu(argumentPlace int) (int,error) {
+	var argument string
+	var selectedMenu int
+	if CheckArgument(argumentPlace) {
+		argument = os.Args[argumentPlace]
+		if argument != "" {
+			var err error
+			selectedMenu, err = strconv.Atoi(argument)
+			if err != nil {
+				return 0, errors.New("invalid input argument")
+			}
 		}
+		return selectedMenu,nil
 	}
+	return 0,nil
 }
-func Reset(selectedMenu *int) {
-	*selectedMenu = 0
+func Reset() int {
 	var discard string
-	fmt.Scanln(&discard)
+	_, _ = fmt.Scanln(&discard)
+	return 0
 }
 func main()  {
 	var firstNumber,secondNumber,selectedMenu int
-	var arg string
-	CheckArg(&arg, &selectedMenu)
+	
+	selectedMenu,err := ParseArgumentToMenu(1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	for {
 		if selectedMenu == 0 {
-			DisplayMenu()
+			fmt.Println(DisplayMenu())
 			fmt.Print("Please enter command: ")
 			fmt.Scanf("%d\n",&selectedMenu)
 		}
 
+
 		switch constant.MenuType(selectedMenu) {
-		case constant.PLUS: if err :=InputHandler(&firstNumber,&secondNumber); err!=nil{Reset(&selectedMenu);continue}; actions.Plus(firstNumber,secondNumber)
-		case constant.MINUS:if err :=InputHandler(&firstNumber,&secondNumber); err!=nil{Reset(&selectedMenu);continue};  actions.Minus(firstNumber,secondNumber)
+		case constant.PLUS: if err :=InputHandler(&firstNumber,&secondNumber);
+		err!=nil{fmt.Println(err.Error());selectedMenu=Reset();continue};
+		actions.Plus(firstNumber,secondNumber)
+		case constant.MINUS:if err :=InputHandler(&firstNumber,&secondNumber);
+		err!=nil{fmt.Println(err.Error());selectedMenu=Reset();continue};
+		actions.Minus(firstNumber,secondNumber)
 		}
 		selectedMenu = 0
 	}
